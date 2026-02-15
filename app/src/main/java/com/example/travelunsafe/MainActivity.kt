aab4d315bac4bee1467a2d1bc50ee4fa93a3e28a
Plathu28 <premieromggg11@gmail.com>
2026-02-15 15:01:11 +0700

    Add hotel

--- app/src/main/java/com/example/travelunsafe/MainActivity.kt
+++ app/src/main/java/com/example/travelunsafe/MainActivity.kt
@@ -36,6 +36,10 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
+    Text(
+        text = "Hello Premier!",
+        modifier = modifier
+    )
 }
 
 @Preview(showBackground = true)
