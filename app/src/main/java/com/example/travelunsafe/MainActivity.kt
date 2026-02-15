6663d19d4efaa9be47379d1eea8bfe17585ed83a
ApiwatG <apiwat.g@kkumail.com>
2026-02-15 07:59:45 +0700

    เพิ่มtle

--- app/src/main/java/com/example/travelunsafe/MainActivity.kt
+++ app/src/main/java/com/example/travelunsafe/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             TravelUnSafeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Apiwat",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
