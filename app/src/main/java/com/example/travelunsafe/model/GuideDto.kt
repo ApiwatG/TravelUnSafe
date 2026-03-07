7f6efa77e12ce54749ccaa5a79c5d0ed191afa43
Kiattipoomp0907 <kiattipoom.s@kkumail.com>
2026-03-07 19:05:36 +0700

    add new CreateGuideActivity.kt 02

--- app/src/main/java/com/example/travelunsafe/model/GuideDto.kt
+++
@@ -1,9 +0,0 @@
-package com.example.travelunsafe.model
-
-data class GuideDto(
-    val guide_id: String,
-    val guide_name: String,
-    val username: String,
-    val provinces_name: String,
-    val guide_detail: String? = null
-)
